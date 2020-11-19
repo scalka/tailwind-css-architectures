7f4d3df620cba4878c9e10ddee87e67244a14ad9
Sylwia Calka <calka.sylwia@gmail.com>
2020-11-19 11:19:38 +0100

    delete unused components

--- src/css-in-css/index.js
+++
@@ -1,4 +0,0 @@
-import Scss from "./css-with-sass";
-import CssModules from "./css-modules";
-
-export default { Scss, CssModules };
--- src/css-in-js/index.js
+++
@@ -1,3 +0,0 @@
-import StyledComponents from './styled-components';
-
-export default { StyledComponents };
